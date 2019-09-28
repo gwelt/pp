f120661905a9010e4ac17b7b3f97eb1f92a3d63e
sven <sven@gwelt.net>
2019-09-28 21:58:40 +0200

    update on change only

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ var path = require('path');
 var PORT = process.env.PORT || 3008;
 server.listen(PORT, function() {process.stdout.write(`\x1b[44m SERVER LISTENING ON PORT ${ PORT } \x1b[0m \n`)});
 app.use('/:path?/:id?/manifest.json', function (req, res) {
-  //let id=(req.params.id)||'';
-  res.json({
-    "short_name": (req.params.id?''+req.params.id+' planning poker':'planning poker'),
-    "name": (req.params.id?''+req.params.id+' planning poker':'planning poker'),
-    "icons": [
-      {"src": "/images/pp192.png","sizes": "192x192","type": "image/png"},
-      {"src": "/images/pp512.png","sizes": "512x512","type": "image/png"},
-      {"src": "/images/pp1024.png","sizes": "1024x1024","type": "image/png"}
-    ],
-    "start_url": '/'+(req.params.path?req.params.path+'/':'')+(req.params.id||''),
-    "background_color": "#000",
-    "theme_color": "#000",
-    "display": "standalone"
-  });
+	//let id=(req.params.id)||'';
+	res.json({
+		"short_name": (req.params.id?''+req.params.id+' planning poker':'planning poker'),
+		"name": (req.params.id?''+req.params.id+' planning poker':'planning poker'),
+		"icons": [
+			{"src": "/images/pp192.png","sizes": "192x192","type": "image/png"},
+			{"src": "/images/pp512.png","sizes": "512x512","type": "image/png"},
+			{"src": "/images/pp1024.png","sizes": "1024x1024","type": "image/png"}
+		],
+		"start_url": '/'+(req.params.path?req.params.path+'/':'')+(req.params.id||''),
+		"background_color": "#000",
+		"theme_color": "#000",
+		"display": "standalone"
+	});
 });
 app.use(function(req,res) {res.sendFile(path.join(__dirname,'public','index.html'))});
 let default_cards = '?,0,1,2,3,5,8,13,20';
 
 io.on('connection', function (socket) {
-  join(socket,socket.client.request.headers.referer.match(/[^\/]\/([^\/]*)\/?$/i)[1].toLowerCase());
-  // socket.emit = reply only to the client who asked
-  // socket.broadcast.emit = reply to all clients except the one who asked
-  // io.sockets.emit = reply to all clients (including the one who asked)
-  socket.on('join', function (room) {join(socket,room)});
-  socket.on('set', function (name,vote) {set(socket,name,vote)});
-  socket.on('show', function () {show(socket.current_room)});
-  socket.on('reset', function () {reset(socket.current_room)});
-  socket.on('set_room_cards', function (cards) {set_room_cards(socket.current_room,cards)});
-  socket.on('leave', function () {leave(socket,socket.current_room)});
-  socket.on('disconnect', function () {leave(socket,socket.current_room)});
+	join(socket,socket.client.request.headers.referer.match(/[^\/]\/([^\/]*)\/?$/i)[1].toLowerCase());
+	// socket.emit = reply only to the client who asked
+	// socket.broadcast.emit = reply to all clients except the one who asked
+	// io.sockets.emit = reply to all clients (including the one who asked)
+	socket.on('join', function (room) {join(socket,room)});
+	socket.on('set', function (name,vote) {set(socket,name,vote)});
+	socket.on('show', function () {show(socket.current_room)});
+	socket.on('reset', function () {reset(socket.current_room)});
+	socket.on('set_room_cards', function (cards) {set_room_cards(socket.current_room,cards)});
+	socket.on('leave', function () {leave(socket,socket.current_room)});
+	socket.on('disconnect', function () {leave(socket,socket.current_room)});
 });
 
 var rooms=[];
@@ -78,7 +78,9 @@ function leave(socket,room) {
 function set(socket,name,vote) {
 	socket.name=(name!==''?name:undefined);
 	socket.vote=(vote!==''?vote:undefined);
-	update_about_match(socket.current_room);
+	if ((socket.name!==name)||(socket.vote!==vote)) {
+		update_about_match(socket.current_room);
+	}
 }
 
 function show(room) {
